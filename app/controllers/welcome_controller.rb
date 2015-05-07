b866684a96a122e955f6bf6fa1fdcfdd79e5bcba
DJDeWitt <djdewitt@gmail.com>
2015-05-07 12:11:08 -0500

    more landing page styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,8 @@ class WelcomeController < ApplicationController
 
   def index
     @user = User.new
-    @posts = Post.search(params[:search], params[:page]).take(8)
-    @projects = Project.search(params[:search], params[:page]).take(8)
+    @posts = Post.search(params[:search], params[:page]).take(3)
+    @projects = Project.search(params[:search], params[:page]).take(3)
   end
 
 end
